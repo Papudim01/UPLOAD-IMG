3e31bb351c2116ec66111cc2e70a93808087d826
LUIZ GUSTAVO DO NASCIMENTO SOUSA <luiz.gnsousa@senacsp.edu.br>
2025-04-10 13:56:00 -0300

    projeto img

---
+++ db.js
@@ -0,0 +1,27 @@
+// Importa para interagir com o DB
+const mongoose = require("mongoose");
+
+// Carrega variaveis de ambiente do arquivo .env
+require("dotenv").config();
+
+// Configurando para permitir consultas (restritas)
+mongoose.set("strictQuery", true);
+
+// obtem as crendencias do Mongodb armazenada no .env
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+// Função para no DB
+async function main() {
+    await mongoose
+        .connect
+        // link do mongo
+        (`mongodb+srv://${dbUser}:${dbPassword}@cluster0.b5njy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    // Exibe ao usuario que realizou a conexão 
+    console.log("Conectou ao banco de dados!");
+}
+// Exibe a msg ao usuario com erro 
+main().catch((err) => console.log(err));
+
+// Exporta a função para utilizar em outro arquivo
+module.exports = main;
---
+++ routes/picture.js
@@ -0,0 +1,23 @@
+// Importa o módulo do Express para configurar as Rotas
+const express = require("express");
+
+// Cria a instância do roteador do express para definir ROTAS
+const router = express.Router();
+
+// Importa a configuração do Multer para ligar com uploads de arquivos
+const upload = require("../config/multer");
+
+// Importa o controlador da IMg, onde tem todas as funções e busca
+const PictureController = require("../controllers/PictureController");
+
+// Definindo a rota POST para criar, e fazer upload da imagem
+router.post("/", upload.single("file"), PictureController.create);
+
+// Definindo a rota GET para buscar todas as imagens do DB
+router.get("/", PictureController.findAll);
+
+// Rota para obter uma imagem específica
+router.get("/:id/image", PictureController.getImage);
+
+// Exportando o arquivo para utilizar no app.js
+module.exports = router;
